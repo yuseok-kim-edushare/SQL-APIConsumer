b07ba3b88ac96c3071619ac4d86f58d7eff80e59
김유석(Yu Seok Kim) <yuseok.kim@vilac.co.kr>
2025-03-18 14:59:30 +0900

    correct version number suggestion

--- API_Consumer/Properties/AssemblyInfo.cs
+++ API_Consumer/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyVersion("2.3.7.0")]
+[assembly: AssemblyFileVersion("2.3.7.0")]
